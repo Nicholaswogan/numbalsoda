a6dcba849d726e0aff1ee71ae419dbb97afc18b8
Nicholaswogan <wogan@uw.edu>
2021-07-27 07:37:11 -0700

    update setup.py description

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from skbuild import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="NumbaLSODA",
     packages=['NumbaLSODA'],
@@ -10,5 +15,7 @@ setup(
     author_email = 'nicholaswogan@gmail.com',
     description = 'Python wrapper of LSODA (solving ODEs)'+\
                   ' which can be called from within numba functions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     cmake_args=['-DSKBUILD=ON']
     )
