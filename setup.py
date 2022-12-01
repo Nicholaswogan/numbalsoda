bc4756f768b6338dde09e7ddfb5b9de927f81698
tbridel <thibault.bridelbertomeu@gmail.com>
2022-12-01 22:49:02 +0100

    stump for rk merson implementation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name="numbalsoda",
     packages=['numbalsoda'],
-    version='0.3.5',
+    version='0.3.6.dev0',
     license='MIT',
     install_requires=['numpy','numba'],
     author = 'Nicholas Wogan',
